908931606bb99ae595620045df8e324381122542
Jairo Alberto Morelli <40253573+Jairo411@users.noreply.github.com>
2024-10-30 16:44:51 -0400

    first commit

---
+++ build/CMakeFiles/tutorial-binary.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/jalbm/Desktop/Developer/cmake_playground/cmake-3.27.0-tutorials/Step1/tutorial.cxx" "CMakeFiles/tutorial-binary.dir/tutorial.cxx.obj" "gcc" "CMakeFiles/tutorial-binary.dir/tutorial.cxx.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
